4c894abc61fb8a87d59289bc8807ba825160d0c7
kai <282145321@qq.com>
2020-09-21 17:51:44 +0800

    资源替换

--- jiayuan-master/common/js/metadata.js
+++ jiayuan-master/common/js/metadata.js
@@ -2,12 +2,12 @@
  * 分享给好友
  * @param {Object} res
  */
-function shareAppMessage (res) {
-    return {
-  	  title: 'xxxxxx结婚典礼请柬',
-  	  desc: '真诚的邀请您的光临',
-      path: '/pages/index/index'
-    }
+function shareAppMessage(res) {
+	return {
+		title: '朱凯黄叶子结婚典礼请柬',
+		desc: '真诚的邀请您的光临',
+		path: '/pages/index/index'
+	}
 }
 /**
  * 酒店gps定位，参考
@@ -15,45 +15,52 @@ function shareAppMessage (res) {
  * latitude 是纬度，国内一般是北纬
  * longitude 是经度，国内一般是东经
  */
-function geo () {
-    return {
-        latitude: 36.670757,
-        longitude: 120.984320
-    }
+function geo() {
+	return {
+		latitude: 27.828516,
+		longitude: 112.924339
+	}
+}
+
+function geo1() {
+	return {
+		latitude: 28.915184,
+		longitude: 110.125961
+	}
 }
 /**
  * 新郎新娘电话
  * he 新郎
  * she 新娘
  */
-function call () {
-    return {
-        he: '15555555555',
-        she: '16666666666'
-    }
+function call() {
+	return {
+		he: '17620378878',
+		she: '15013849739'
+	}
 }
 /**
  * 小程序云开发环境id
  */
-function env () {
-    return 'xxx'
+function env() {
+	return 'xiaozhuyezi-5gt1yxnvf429b9bd'
 }
 /**
  * 我这个版本默认把视频去掉了
  * src 是婚礼视频的地址
  * poster 是封面
  */
-function video () {
-    return  {
-        src: 'http://xx.mp4',
-        poster: 'http://xx.jpg'
-    }
+function video() {
+	return {
+		src: 'http://xx.mp4',
+		poster: 'http://xx.jpg'
+	}
 }
 
 export default {
-  shareAppMessage,
-  geo,
-  call,
-  env,
-  video
+	shareAppMessage,
+	geo,
+	call,
+	env,
+	video
 }
